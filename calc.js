34e82463bfc70cb958a19ec3e93a17f1900739c8
naasirosmanzoro <naasir.osman@zoro.co.uk>
2022-08-16 16:14:47 +0100

    working calculator - not for long functions

--- calc.js
+++ calc.js
@@ -3,107 +3,124 @@
 
 
 var sevenNumberButton = document.querySelector(".sevenbtn")
-//console.log('btn', numberButton.innerHTML)
 
-sevenNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML += document.querySelector(".sevenbtn").innerHTML
-})
-
-
-
-
-
+    sevenNumberButton.addEventListener("click", function() {
+        if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
+        document.querySelector(".sum-box").innerHTML = document.querySelector(".sevenbtn").innerHTML
+        }
+        else {
+            document.querySelector(".sum-box").innerHTML += document.querySelector(".sevenbtn").innerHTML
+    }})
 
 
-//var sumBox = parseInt(document.querySelector(".sum-box").innerHTML) 
-//console.log(sumBox)
 var eightNumberButton = document.querySelector(".eightbtn")
 
     eightNumberButton.addEventListener("click", function() {
         if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
         document.querySelector(".sum-box").innerHTML = document.querySelector(".eightbtn").innerHTML
-        console.log(parseInt(document.querySelector(".sum-box").innerHTML))
-        console.log(parseInt(document.querySelector(".sum-box").innerHTML))
         }
         else {
             document.querySelector(".sum-box").innerHTML += document.querySelector(".eightbtn").innerHTML
  }})
+ 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-var nineNumberButton = document.querySelector(".ninebtn")
-
-nineNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML += document.querySelector(".ninebtn").innerHTML
-})
-
-
+ var nineNumberButton = document.querySelector(".ninebtn")
+    nineNumberButton.addEventListener("click", function() {
+    if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
+            document.querySelector(".sum-box").innerHTML = document.querySelector(".ninebtn").innerHTML 
+    }
+    else {
+        document.querySelector(".sum-box").innerHTML += document.querySelector(".ninebtn").innerHTML
+    }
+        })
+    
 
 var fourNumberButton = document.querySelector(".fourbtn")
+        fourNumberButton.addEventListener("click", function() {
+        if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
+                document.querySelector(".sum-box").innerHTML = document.querySelector(".fourbtn").innerHTML 
+        }
+        else {
+            document.querySelector(".sum-box").innerHTML += document.querySelector(".fourbtn").innerHTML
+        }
+            })
+        
 
-fourNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML += document.querySelector(".fourbtn").innerHTML
-})
 
 
 
 var fiveNumberButton = document.querySelector(".fivebtn")
-
-fiveNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML += document.querySelector(".fivebtn").innerHTML
-})
-
-
-
-var sixNumberButton = document.querySelector(".sixbtn")
-
-sixNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML += document.querySelector(".sixbtn").innerHTML
-})
+            fiveNumberButton.addEventListener("click", function() {
+            if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
+                    document.querySelector(".sum-box").innerHTML = document.querySelector(".fivebtn").innerHTML 
+            }
+            else {
+                document.querySelector(".sum-box").innerHTML += document.querySelector(".fivebtn").innerHTML
+            }
+                })
 
 
 
-var oneNumberButton = document.querySelector(".onebtn")
-
-oneNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML += document.querySelector(".onebtn").innerHTML
-})
-
-
-
-var twoNumberButton = document.querySelector(".twobtn")
-
-twoNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML += document.querySelector(".twobtn").innerHTML
-})
+ var sixNumberButton = document.querySelector(".sixbtn")
+    sixNumberButton.addEventListener("click", function() {
+    if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
+            document.querySelector(".sum-box").innerHTML = document.querySelector(".sixbtn").innerHTML 
+    }
+    else {
+        document.querySelector(".sum-box").innerHTML += document.querySelector(".sixbtn").innerHTML
+    }
+        })
 
 
 
-var threeNumberButton = document.querySelector(".threebtn")
+ var oneNumberButton = document.querySelector(".onebtn")
+    oneNumberButton.addEventListener("click", function() {
+    if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
+            document.querySelector(".sum-box").innerHTML = document.querySelector(".onebtn").innerHTML 
+    }
+    else {
+        document.querySelector(".sum-box").innerHTML += document.querySelector(".onebtn").innerHTML
+    }
+        })
 
-threeNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML += document.querySelector(".threebtn").innerHTML
-})
 
 
 
-var zeroNumberButton = document.querySelector(".zerobtn")
+ var twoNumberButton = document.querySelector(".twobtn")
+    twoNumberButton.addEventListener("click", function() {
+    if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
+            document.querySelector(".sum-box").innerHTML = document.querySelector(".twobtn").innerHTML 
+    }
+    else {
+        document.querySelector(".sum-box").innerHTML += document.querySelector(".twobtn").innerHTML
+    }
+        })
 
-zeroNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML += document.querySelector(".zerobtn").innerHTML
-})
+
+
+
+ var threeNumberButton = document.querySelector(".threebtn")
+    threeNumberButton.addEventListener("click", function() {
+    if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
+            document.querySelector(".sum-box").innerHTML = document.querySelector(".threebtn").innerHTML 
+    }
+    else {
+        document.querySelector(".sum-box").innerHTML += document.querySelector(".threebtn").innerHTML
+    }
+        })
+
+
+
+ var zeroNumberButton = document.querySelector(".zerobtn")
+    zeroNumberButton.addEventListener("click", function() {
+    if (parseInt(document.querySelector(".sum-box").innerHTML) === 0) {
+            document.querySelector(".sum-box").innerHTML = document.querySelector(".zerobtn").innerHTML 
+    }
+    else {
+        document.querySelector(".sum-box").innerHTML += document.querySelector(".zerobtn").innerHTML
+    }
+        })
 
 
 
@@ -111,15 +128,6 @@ var pointNumberButton = document.querySelector(".pointbtn")
 pointNumberButton.addEventListener("click", function() {
     document.querySelector(".sum-box").innerHTML += document.querySelector(".pointbtn").innerHTML
 })
-
-
-
-var acNumberButton = document.querySelector(".acbtn")
-
-acNumberButton.addEventListener("click", function() {
-    document.querySelector(".sum-box").innerHTML = document.querySelector(".zerobtn").innerHTML
-})
-
 
 
 var plusMinusNumberButton = document.querySelector(".plusminusbtn")
@@ -209,62 +217,9 @@ minusNumberButton.addEventListener("click", function() {
     var equalNumberButton = document.querySelector(".equalbtn")
     
     equalNumberButton.addEventListener("click", function() {
-       document.querySelector(".sum-box").innerHTML = parseInt(preMinusOperation) * 100 / parseInt(document.querySelector(".sum-box").innerHTML)
+       document.querySelector(".sum-box").innerHTML = parseInt(preMinusOperation) - parseInt(document.querySelector(".sum-box").innerHTML)
     })
 
 })
-
-
-
-
-
-
-/* let simonText = document.getElementById("simon").innerHTML;
-// let david = document.getElementById("david").innerHTML  // this is only selecting 7
-const davidBtn = document.getElementById("david")
-console.log(simon)
-//console.log(david)
-console.log(davidBtn)
-
-davidBtn.addEventListener("click", function() {
-    document.getElementById("simon").innerHTML += "david";
-  });
-
-console.log(simon) */
-
-
-
-
-
-//  btn.querySelector('.btn').addEventListener('click', function() {
-//                sum.innerText = btn.value;});
-
-//const btn = document.querySelector(".numbtn");
-//btn.addEventListener("click", function() {
-//document.querySelector(".sum-box").innerHTML = btn.value; });
-
-/* let sum = 0
-const btn = document.querySelector(".numbtn");
-
-if (btn.addEventListener("click", function() {
-    sum += btn.value
-
-}))
-console.log(sum)
-document.querySelector(".sum-box").innerHTML = sum; */
-
-//let sum = 0
-//console.log(sum)
-
-//let helloElement = document.getElementById("hello").innerHTMl
-//let byeElement = document.getElementById("bye").innerHTML
-
-//console.log(helloElement)
-//console.log(byeElement)
-
-//helloElement = byeElement;
-
-//console.log(byeElement.value)
-
 
 
